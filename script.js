e80571b9a91987c79ffea7d4ab3029f7f776616e
cainamicael <cainamicaeloficial@gmail.com>
2023-05-25 15:09:40 -0300

    adicionada a opção de quantidade

--- script.js
+++ script.js
@@ -4,35 +4,53 @@ document.querySelector('input[type=submit]')
     .addEventListener('click', () => {
         var nomeProduto = document.querySelector('input[name=nome_produto]')
         var precoProduto = document.querySelector('input[name=valor_produto]')
+        var quantidadeProduto = document.querySelector('input[name=quantidade_produto]')
 
-        if(nomeProduto.value != '' && precoProduto.value != '') {
-            itens.push({ //Pega os valores do input e adiciona no array
-                nome: nomeProduto.value,
-                valor: parseFloat(precoProduto.value)
-            })
-    
-            //Adicionando dinamicamente os itens
-            var listaProdutos = document.querySelector('.lista-produtos')
-            var somatorio = itens.map(i => i.valor).reduce((a, c) => a + c)
-            listaProdutos.innerHTML = '' //Para não ficar duplicando
-    
-            itens.forEach((val, index) => {
-                listaProdutos.innerHTML += `
-                    <div class="lista-produto-single">
-                        <h3>${val.nome}</h3>
-                        <h3 class="price-produto"><span>R$${(val.valor).toFixed(2)}</span></h3>
-                        <button class="remover" onclick="removerItem(${index})">remover</button>
-                    </div>`
-            })
-    
+        var quantidade = parseInt(quantidadeProduto.value)
+
+        if(isNaN(quantidade)) {
+            quantidade = 1
+        }
+
+        if(nomeProduto.value != '' && precoProduto.value != '' && quantidade >= 1) {
+            for(var i = 0; i < quantidade; i++) {
+                itens.push({ //Pega os valores do input e adiciona no array
+                    nome: nomeProduto.value,
+                    valor: parseFloat(precoProduto.value)
+                })
+        
+                //Adicionando dinamicamente os itens
+                var listaProdutos = document.querySelector('.lista-produtos')
+                var somatorio = itens.map(i => i.valor).reduce((a, c) => a + c)
+                listaProdutos.innerHTML = '' //Para não ficar duplicando
+        
+                itens.forEach((val, index) => {
+                    listaProdutos.innerHTML += `
+                        <div class="lista-produto-single">
+                            <h3>${val.nome}</h3>
+                            <h3 class="price-produto"><span>R$${(val.valor).toFixed(2)}</span></h3>
+                            <button class="remover" onclick="removerItem(${index})">remover</button>
+                        </div>`
+                })
+            }
+        
+                //Limpando os inputs
+                nomeProduto.value = ''
+                precoProduto.value = ''
+                quantidadeProduto.value = ''
+        
+                let elementoSoma = document.querySelector('.soma-produto h1')
+                elementoSoma.innerHTML = `Total: R$ ${somatorio.toFixed(2)}`
+
+            
+            
+        } else {
+            alert('Insira todos os valores para continuar')
+
             //Limpando os inputs
             nomeProduto.value = ''
             precoProduto.value = ''
-    
-            let elementoSoma = document.querySelector('.soma-produto h1')
-            elementoSoma.innerHTML = `Total: R$ ${somatorio.toFixed(2)}`
-        } else {
-            alert('Insira todos os valores para continuar')
+            quantidadeProduto.value = ''
         }
         
     })
